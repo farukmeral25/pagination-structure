385913379af260e232b4b32392cc537c9576221e
Ömer Faruk MERAL <omer.meral@path.com.tr>
2024-08-03 13:30:13 +0300

    Add info comment to IHomeService

--- lib/feature/home/domain/service/i_home_service.dart
+++ lib/feature/home/domain/service/i_home_service.dart
@@ -3,5 +3,6 @@ import 'package:pagination_structure/core/utils/ui_state.dart';
 import 'package:pagination_structure/feature/home/domain/dto/task_dto.dart';
 
 abstract class IHomeService {
+  //We can't use UIState, we usually use dartz or etc. Either<Failure,T> infra
   Future<UIState<PaginationDto<TaskDto>>> fetchTasks();
 }
